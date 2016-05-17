690412e96e9c1ec3d6fa162e45df148925d48836
Amelia Kaufman <amelia.kaufman@gmail.com>
2016-05-17 15:14:31 -0400

    practicing callback functions and grabbing data from apis

---
+++ example-callback.js
@@ -0,0 +1,16 @@
+var request = require('request');
+var url = 'http://api.openweathermap.org/data/2.5/weather?q=Philadelphia&units=imperial&appid=7cece7e7ad0e01a2f316e521d4a09867'
+
+request({
+	url: url,
+	// want to request json data
+	json: true
+}, function(error, response, body){
+	if (error) {
+		console.log('Unable to fetch weather.');
+	} else {
+		// 4 = number of indents in formatting
+		console.log(JSON.stringify(body, null, 4));
+		console.log('It\'s ' + body.main.temp + ' in ' + body.name + '!');
+	}
+})

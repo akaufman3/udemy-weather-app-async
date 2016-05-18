fe5371396f3f2dcb48b08a6262a1b1b4d4b07869
Amelia Kaufman <amelia.kaufman@gmail.com>
2016-05-18 17:50:30 -0400

    fetch a location using promises and return weather to user for current location

--- location.js
+++ location.js
@@ -2,18 +2,20 @@ var request = require('request');
 // a way to fetch a users location based on the IP address
 var url = 'http://ipinfo.io';
 
-module.exports = function(callback) {
-	request({
-		url: url,
-		// want to request json data
-		json: true
-	}, function(error, response, body){
-		if (error) {
-			callback();
-		} else {
-			// 4 = number of indents in formatting
-			// console.log(JSON.stringify(body, null, 4));
-			callback(body);
-		}
+module.exports = function(location) {
+	return new Promise(function(resolve, reject) {
+		request({
+			url: url,
+			// want to request json data
+			json: true
+		}, function(error, response, body){
+			if (error) {
+				reject('Unable to get location.');
+			} else {
+				// 4 = number of indents in formatting
+				// console.log(JSON.stringify(body, null, 4));
+				resolve(body);
+			}
+		});
 	});
-}
+};
--- weather.js
+++ weather.js
@@ -1,25 +1,27 @@
 var request = require('request');
 var key = require('./weatherKeys.js');
 
-module.exports = function(location, callback) {
-	var encodedLocation = encodeURIComponent(location);
-	var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + encodedLocation + '&units=imperial&appid=' + key.api_key;
+module.exports = function(location) {
+	return new Promise(function(resolve, reject) {
+		var encodedLocation = encodeURIComponent(location);
+		var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + encodedLocation + '&units=imperial&appid=' + key.api_key;
 
-	if (!location) {
-		return callback('No location provided');
-	}
-	
-	request({
-		url: url,
-		// want to request json data
-		json: true
-	}, function(error, response, body){
-		if (error) {
-			callback('Unable to fetch weather.');
-		} else {
-			// 4 = number of indents in formatting
-			// console.log(JSON.stringify(body, null, 4));
-			callback('It\'s ' + body.main.temp + ' in ' + body.name + '!');
+		if (!location) {
+			return reject('No location was provided');
 		}
+		
+		request({
+			url: url,
+			// want to request json data
+			json: true
+		}, function(error, response, body){
+			if (error) {
+				reject('Unable to fetch weather.');
+			} else {
+				// 4 = number of indents in formatting
+				// console.log(JSON.stringify(body, null, 4));
+				resolve('It\'s ' + body.main.temp + ' in ' + body.name + '!');
+			}
+		});
 	});
 }

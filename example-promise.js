bf309982cf578f7264a2fccee38c8f0147e14379
Amelia Kaufman <amelia.kaufman@gmail.com>
2016-05-18 16:09:57 -0400

    pracitce writing promises which do something after the data is fetched

--- example-promise.js
+++ example-promise.js
@@ -1,22 +1,58 @@
-// callback
-// function doWork(data, callback) {
-// 	callback('done');
+var request = require('request');
+var key = require('./weatherKeys.js');
+// // callback
+// // function doWork(data, callback) {
+// // 	callback('done');
+// // }
+
+// // promise
+// function doWorkPromise(data) {
+// 	// creating a new promise and returning promise so the caller has access to it
+// 	return new Promise(function(resolve, reject) {
+// 		reject('everything is broken!');
+// 		// reject({
+// 		// 	error: 'something bad happened'
+// 		// });
+// 	});
 // }
 
-// promise
-function doWorkPromise(data) {
-	// creating a new promise and returning promise so the caller has access to it
+// // get the data then do this callback
+// doWorkPromise('some data').then(function(data) {
+// 	console.log(data);
+// }, function(error){
+// 	console.log(error);
+// });
+
+function getWeather(location) {
 	return new Promise(function(resolve, reject) {
-		reject('everything is broken!');
-		// reject({
-		// 	error: 'something bad happened'
-		// });
+		var encodedLocation = encodeURIComponent(location);
+		var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + encodedLocation + '&units=imperial&appid=' + key.api_key;
+
+		if (!location) {
+			return reject('No locatoin was provided');
+		}
+		
+		request({
+			url: url,
+			// want to request json data
+			json: true
+		}, function(error, response, body){
+			if (error) {
+				reject('Unable to fetch weather.');
+			} else {
+				// 4 = number of indents in formatting
+				// console.log(JSON.stringify(body, null, 4));
+				resolve('It\'s ' + body.main.temp + ' in ' + body.name + '!');
+			}
+		});
 	});
 }
 
-// get the data then do this callback
-doWorkPromise('some data').then(function(data) {
-	console.log(data);
-}, function(error){
+
+getWeather('san francisco').then(function(currentWeather) {
+	console.log(currentWeather);
+}, function (error) {
 	console.log(error);
 });
+
+
